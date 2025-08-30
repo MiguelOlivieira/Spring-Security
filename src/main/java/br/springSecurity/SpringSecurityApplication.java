92d9a192f828495a02c5485b1fb114e492f173f3
Miguel Vieira <152816803+MiguelOlivieira@users.noreply.github.com>
2025-08-30 17:55:36 -0300

    UPDATE: Criptografia adicionada

--- src/main/java/br/springSecurity/SpringSecurityApplication.java
+++ src/main/java/br/springSecurity/SpringSecurityApplication.java
@@ -4,6 +4,9 @@ package br.springSecurity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class SpringSecurityApplication {
@@ -11,5 +14,14 @@ public class SpringSecurityApplication {
     public static void main(String[] args){
         SpringApplication.run(SpringSecurityApplication.class, args);
     }
+
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+
+
 
 }
--- src/main/java/br/springSecurity/controller/UsuarioController.java
+++ src/main/java/br/springSecurity/controller/UsuarioController.java
@@ -3,19 +3,24 @@ package br.springSecurity.controller;
 
 import br.springSecurity.model.UsuarioModel;
 import br.springSecurity.repository.UsuarioRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/usuario")
 public class UsuarioController{
 
     private final UsuarioRepository repository;
+    private final PasswordEncoder encoder;
 
-    public UsuarioController(UsuarioRepository  repository){
+    public UsuarioController(UsuarioRepository  repository, PasswordEncoder encoder ){
         this.repository = repository;
+        this.encoder = encoder;
     }
 
 
@@ -27,9 +32,29 @@ public class UsuarioController{
 
     @PostMapping("/salvar")
     public ResponseEntity<UsuarioModel> salvar(@RequestBody UsuarioModel usuario) {// permite receber um json e converter para um objeto
-
+        String hashedPassword = encoder.encode(usuario.getPassword());
+        usuario.setPassword(hashedPassword);
 
         return ResponseEntity.ok(repository.save(usuario));
+
+    }
+
+    @GetMapping("/validarSenha")
+    public ResponseEntity<Boolean> validarSenha(@RequestParam String login,
+                                                @RequestParam String password) {
+
+        Optional<UsuarioModel> optUsuario = repository.findByLogin(login);
+
+        if (optUsuario.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(false);
+        }
+
+        UsuarioModel usuario = optUsuario.get();
+        boolean valid = encoder.matches(password, usuario.getPassword());
+
+        HttpStatus status = (valid) ? HttpStatus.OK : HttpStatus.UNAUTHORIZED;
+
+        return ResponseEntity.status(status).body(valid);
 
     }
 }
